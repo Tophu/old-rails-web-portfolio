176874ce4022b9e2156b946bf39a23d227833058
Phuong To <tophu95@gmail.com>
2019-04-09 16:38:46 +0100

    added JS to side navbar, positioned footer properly

---
+++ app/javascript/components/navbar.js
@@ -0,0 +1,34 @@
+const togglebtns = document.querySelectorAll('.nav-toggle');
+const sideBar = document.querySelector('div.side-navbar');
+const sidebarLinks = document.querySelectorAll('.side-navbar li');
+const threeDotMenu = document.querySelector('div.nav-menu-icon');
+const navMenu = document.querySelector('div.nav-menu');
+const backgroundFader = document.getElementsByClassName('background-fader')[0];
+
+
+function toggleSidebar() {
+  sideBar.classList.toggle('show-side-navbar');
+  backgroundFader.classList.toggle('fade-background');
+}
+
+togglebtns.forEach(togglebtn => {
+  togglebtn.onclick = function() {
+    toggleSidebar();
+  };
+});
+
+backgroundFader.onclick = () => {
+  toggleSidebar(); // this is much easier to understand
+};
+
+// Handle clicks on li element inside navigation sidebar
+sidebarLinks.forEach(link => {
+  link.onclick = e => {
+    e.preventDefault();
+    toggleSidebar();
+  };
+});
+
+threeDotMenu.onclick = function() {
+  navMenu.classList.toggle('show-nav-menu');
+};
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,2 @@
 import "bootstrap";
+import { toggleSidebar } from '../components/navbar';
