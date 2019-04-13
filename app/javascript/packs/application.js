b02e48a7322abb69b26442eef2c51ac0385ae150
Phuong To <tophu95@gmail.com>
2019-04-13 16:36:42 +0100

    removed colour from default navbar, experimenting with JS and jQuery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,22 +1,3 @@
 import "bootstrap";
 import { toggleSidebar } from '../components/navbar';
-
-
-// $( document ).ready(function() {
-//     const fade = (elementId) => {
-//       $("#home-container").fadeOut("fast", function() {
-//         $("#home-container").html($("#"+elementId).html());
-//         $("#home-container").fadeIn("slow");
-//       });
-//     }
-
-//     fade("main-body");
-
-//     $("#home").click(function(){
-//       fade("main-body");
-//     });
-
-//     $("#about_me").click(function(){
-//       fade("about_me_page");
-//     });
-// });
+// import { toggleHome } from '../components/home';
